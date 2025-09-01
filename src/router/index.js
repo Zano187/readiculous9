bf08e77d96afdb38600bbb13c3f2fd1a3be29ed0
SiyasangaMkwasi22 <mkwasisiya@gmail.com>
2025-09-01 23:03:29 +0200

    LIBRARY

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/home.vue";
 import ForgotPassword from "@/views/ForgotPassword.vue";
 import LoginPage from "@/views/LoginPage.vue";
-
+import Library from "@/views/Library.vue";
 
 
 const routes = [
     {path: '/Home',name: 'Home', component: Home},
     {path: '/ForgotPassword',name: 'ForgotPassword', component: ForgotPassword},
     {path: '/Login', name: 'Login', component: LoginPage},
+    {path: '/Library', name: 'Library', component: Library},
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+
+
 
 export default router
